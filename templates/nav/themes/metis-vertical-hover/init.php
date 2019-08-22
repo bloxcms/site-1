b1a7ffde93d481d0300b14fadb46987ccf46d2fd
bloxcms <vlad@blox.ru>
2019-08-22 11:40:37 +0300

    no metis

--- templates/nav/themes/metis-vertical-hover/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-# TODO  Добавить проверку 'levelsScheme' => 'SSS', то есть все уровни открыты
-
-Blox::addToHead('templates/nav/themes/metis-vertical-hover/nav.css');
-Blox::addToHead('templates/nav/onokumus/metismenu/dist/metisMenu.min.css');    
-Blox::addToFoot('templates/nav/onokumus/metismenu/dist/metisMenu.min.js');
-Blox::addToFoot('<script>$(function(){$("#block-'.$block.' .metismenu").metisMenu({preventDefault: false})})</script>');
-return []; # Для $themeFeatures. пока не используется
