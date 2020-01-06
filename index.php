e0d0e26d99ea65b2e2c36086fcdf4343681435d6
bloxcms <vlad@blox.ru>
2020-01-06 12:42:14 +0300

    metis

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ $dbuser = 'root';
 $dbpass = '';
 $nocache = true;
 $transliterate = true;
-$bloxdir = __DIR__.'/BLOX'; 
-#$bloxdir = 'D:\Programs\OSPanel\domains\seven\BLOX.14\dist';
-#$bloxurl = 'http://192.168.1.199/BLOX.14/dist';
+##$bloxdir = __DIR__.'/BLOX'; 
+$bloxdir = 'D:\Programs\OSPanel\domains\seven\BLOX.14\dist';
+$bloxurl = 'http://192.168.1.199/BLOX.14/dist';
 include $bloxdir.'/go.php';
 		
 		
